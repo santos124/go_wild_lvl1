a64094276b2559df030bb22731d908ae3af61736
wadina <al.stavd@gmail.com>
2021-07-08 19:31:49 +0300

    full5

--- pj35.go
+++ pj35.go
@@ -4,16 +4,14 @@ import (
 )
 // АААBBCCCDDEEE -> 3A2B3C2D3E
 func main() {
-	s := "qaabbccc"
-	s2 := []byte(s)
-	var s3 []byte = []byte{}
+	s := "АААBBCCCDDEEE"
+	s2 := []rune(s)
+	var s3 []rune = []rune{}
 	var count int = 0
 	for i := 0; i < len(s2); i++ {
 		for j := i; j < len(s2); j++ {
 			if s2[i] != s2[j]{
-				fmt.Printf("s3=%s, s2[i]=%v, i=%d, j=%d, count=%v\n", s3, s2[i], i, j, byte(count + 48))
-				s3 = append(s3, byte(count + '0'))
-				
+				s3 = append(s3, rune(count + '0'))
 				s3 = append(s3, s2[i])
 				i = i + count -1
 				count = 0
@@ -21,9 +19,15 @@ func main() {
 			} else {
 				count++
 			}
-			
+			if count != 0  && j == len(s2) - 1{
+				s3 = append(s3, rune(count + '0'))
+				s3 = append(s3, s2[i])
+				i = i + count -1
+				count = 0
+				j = len(s2)
+			}
 		}
-		
 	}
-	fmt.Printf("s=%s, s2=%s, s3=%s\n", s, s2, s3)
+	s4 := string(s3)
+	fmt.Printf("s4=%s\n",s4)
 }
