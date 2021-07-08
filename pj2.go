122f9242f25b96ef1a5ba733c02d8f4a18c1b5c0
wadina <al.stavd@gmail.com>
2021-07-08 17:04:32 +0300

    full2

--- pj2.go
+++ pj2.go
@@ -2,21 +2,24 @@
 package main
 import (
 	"fmt"
+	"sync"
 )
-func compute(inArr [5]int, outArr *[5]int, i int) {
-	outArr[i] = inArr[i] * inArr[i]
-}
+
 func main() {
 	var inArr [5]int = [5]int{2, 4, 6, 8, 10}
 	var outArr [5]int
-	ch := make(chan struct{})
-	func() {
-		defer close(ch)
-		for i := range inArr {
-			compute(inArr, &outArr, i)
-		}
-
-	}()
-	<-ch // ждем выполнения анонимной функции
+	wg := new(sync.WaitGroup)
+	mu := new(sync.Mutex)
+	for i := range inArr {
+		wg.Add(1)
+		go func(WG *sync.WaitGroup, MU *sync.Mutex, i int) {
+			defer WG.Done()
+			MU.Lock()
+			outArr[i] = inArr[i] * inArr[i]
+			MU.Unlock()
+			
+			}(wg, mu, i)
+	}
+	wg.Wait()
 	fmt.Println(outArr) //теперь можно печатать
 }
